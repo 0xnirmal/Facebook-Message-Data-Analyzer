590be4fd615560d82090bbf9f4d6ced1ca8383f1
Nirmal Krishnan <nkrishn9@jhu.edu>
2016-12-30 11:57:09 -0500

    added subset feature

--- generate.py
+++ generate.py
@@ -4,13 +4,31 @@ import time
 import jsonpickle
 from convert import Thread, Message
 
+def thread_counter(thread_to_analyze, verbose=True):
+	participant_dict = {}
+	for participant in thread_to_analyze.participants:
+		participant_dict[participant] = 0
+	for message in thread_to_analyze.messages:
+		try:
+			participant_dict[message.user] += 1
+		except KeyError:
+			if message.user + "(Removed or Left Conversation)" in participant_dict:
+				participant_dict[message.user + "(Removed or Left Conversation)"] += 1
+			else:
+				participant_dict[message.user + "(Removed or Left Conversation)"] = 1
+	if verbose:
+		for participant in participant_dict:
+			print('User: {}, Messages Sent: {}'.format(unicode(participant), participant_dict[participant]))
+
+	return participant_dict
+
 def main():
 	print("Loading...(this could take a few minutes)")
 	f = open(sys.argv[1], 'r')
 	threads = jsonpickle.decode(f.read())
 	choice = 0
-	while choice != 3:
-		choice = int(input("1) Analyze Thread\n2) Analyze User\n3) Quit\nInput: "))
+	while choice != 4:
+		choice = int(input("1) Analyze Thread\n2) Analyze User\n3) Analyze Set of Users\n4) Quit\nInput: "))
 		if choice == 1:
 			print("List of Threads: ")
 			for idx in range(len(threads)):
@@ -26,20 +44,17 @@ def main():
 				print("Invalid input.")
 				sys.exit(-1)
 			thread_to_analyze = threads[index]
-			participant_dict = {}
-			for participant in thread_to_analyze.participants:
-				participant_dict[participant] = 0
-			for message in thread_to_analyze.messages:
-				try:
-					participant_dict[message.user] += 1
-				except KeyError:
-					if message.user + "(Removed or Left Conversation)" in participant_dict:
-						participant_dict[message.user + "(Removed or Left Conversation)"] += 1
-					else:
-						participant_dict[message.user + "(Removed or Left Conversation)"] = 1
-			for participant in participant_dict:
-				print('User: {}, Messages Sent: {}'.format(unicode(participant), participant_dict[participant]))
+			thread_counter(thread_to_analyze)
 			print("\n")
+		elif choice == 2:
+			pass
+		elif choice == 3:
+			input_string = raw_input("Enter a list of users separated by commas with no spaces between one name and the next (ex: Bob Smith,John Doe,Jane Doe): \n")
+			input_set = set(input_string.split(","))
+			print(input_set)
+			for thread in threads:
+				if thread.participants >= input_set:
+					print(thread)
 		# user_to_analyze = raw_input("User to analyze: ")
 		# for message in thread_to_analyze.messages:
 		# 	if message.user == user_to_analyze:
